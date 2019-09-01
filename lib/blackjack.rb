ca3e8bd0b569a133ab9a6d6f028be554dd3a5db2
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-01 02:39:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,54 +1,61 @@
 def welcome
+  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
+  # code #deal_card here
   rand(1..11)
 end
 
-def display_card_total(card_total)
-  puts "Your cards add up to #{card_total}"
+def display_card_total(number)
+  # code #display_card_total here
+  puts "Your cards add up to #{number}"
 end
 
 def prompt_user
+  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def get_user_input
-  answer = gets
+  # code #get_user_input here
+  answer = gets.chomp
   answer
 end
 
-def end_game(card_total)
-  puts "Sorry, you hit #{card_total}. Thanks for playing!"
+def end_game(number)
+  # code #end_game here
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
- card1 = deal_card
- card2 = deal_card
- sum = card1 + card2
- display_card_total(sum)
- return sum
+  # code #initial_round here
+  num1 = deal_card
+  num2 = deal_card
+  display_card_total(num1 + num2)
+  num1 + num2
 end
 
-def hit?(card_total)
-  ask = prompt_user
-  answer = get_user_input
-  
-  while answer != 'h' || 's'
+def hit?(current_card_value)
+  prompt_user
+  user_input = get_user_input
+  while user_input != "h" && user_input != "s"
     invalid_command
     prompt_user
-    answer = get_user_input
-  end 
-  if answer == 'h'
-    card_total += deal_card
-  elsif answer == 's'
-    return card_total
+    user_input = get_user_input
   end
+  if user_input == "h"
+    current_card_value += deal_card
+  end
+  return current_card_value
 end
 
 def invalid_command
+  # code invalid_command here
   puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
@@ -56,10 +63,18 @@ end
 #####################################################
 
 def runner
-  welcome 
-  totalScore = initial_round
-  until totalScore > 21
-    totalScore = hit?(totalScore)
-  end
-  end_game(totalScore)
+  # code runner here
+  welcome
+  total = initial_round
+  until total > 21
+  
+  total = hit?(total)
+  
+  
 end
+end_game(total)
+  
+  
+end
+    
+runner
