da0982aa3530174b496c43e730012e6b21f3cc2d
Matthew Ryan Palmer <matthewpalmer9@outlook.com>
2019-09-01 00:09:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -40,7 +40,7 @@ def hit?(card_total)
     prompt_user
     answer = get_user_input
   if answer = 'h'
-    card_total += 
+    card_total += deal_card
   else 
     invalid_command
     prompt_user
